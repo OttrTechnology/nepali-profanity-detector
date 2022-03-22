f3029978bd41c38cbb2d6a61bd130952b4f0eff6
rubek <rubek@ottr.com.np>
2022-03-22 11:35:26 +0545

    :white_check_mark:  Added tests for throw error behavior

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { hasProfane } from "../src";
+import { hasProfane, Behavior } from "../src";
 
 test("simple profane word", () => {
   expect(hasProfane("muji")).toBe(true);
@@ -14,4 +14,35 @@ test("simple whitelisted word", () => {
 
 test("profane with accents", () => {
   expect(hasProfane("ḿüjį")).toBe(true);
+});
+
+test("profane with throw behaviour", () => {
+  expect(() => hasProfane("muji", { behavior: "THROW_ERR" })).toThrowError();
+});
+
+test("profane with throw behaviour and custom error", () => {
+  class CustomError extends Error {
+    public readonly httpStatusCode: number;
+    public readonly isOperational: boolean;
+
+    constructor(
+      message: string,
+      httpStatusCode: number,
+      isOperational: boolean = false
+    ) {
+      super(message);
+
+      this.httpStatusCode = httpStatusCode;
+      this.isOperational = isOperational;
+    }
+  }
+
+  const error = new CustomError("sth went wrong", 400, true);
+
+  expect(() => {
+    hasProfane("muji", {
+      behavior: "THROW_ERR",
+      error,
+    });
+  }).toThrowError(error);
 });
